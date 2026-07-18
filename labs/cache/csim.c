4e1ae1b9a02e8b09c14239ce80ce46c821277412
edu292 <eduskoroboatei@gmail.com>
2026-07-18 20:01:52 -0300

    Fixed missing cases

--- labs/cache/csim.c
+++ labs/cache/csim.c
@@ -6,8 +6,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define EMPTY_TAG (~0UL)
-
 typedef struct {
   uint64_t tag;
   uint32_t previous;
@@ -16,9 +14,9 @@ typedef struct {
 
 typedef struct {
   size_t valid_lines;
+  Line *lines;
   uint32_t most_recent;
   uint32_t least_recent;
-  Line *lines;
 } Set;
 
 typedef struct {
@@ -37,17 +35,17 @@ typedef struct {
 Cache *new_cache(size_t set_bits, size_t lines_per_set, size_t block_bits) {
   size_t sets_capacity = 1UL << set_bits;
 
-  size_t cache_sz = sizeof(Cache);
-  size_t sets_sz = sizeof(Set) * sets_capacity;
-  size_t lines_sz = sizeof(Line) * lines_per_set * sets_capacity;
+  size_t cache_size = sizeof(Cache);
+  size_t sets_size = sizeof(Set) * sets_capacity;
+  size_t lines_size = sizeof(Line) * lines_per_set * sets_capacity;
 
-  char *mem = malloc(cache_sz + sets_sz + lines_sz);
+  char *mem = malloc(cache_size + sets_size + lines_size);
   if (!mem)
     return NULL;
 
   Cache *c = (Cache *)mem;
-  c->sets = (Set *)(mem + cache_sz);
-  Line *all_lines = (Line *)(mem + cache_sz + sets_sz);
+  c->sets = (Set *)(mem + cache_size);
+  Line *all_lines = (Line *)(mem + cache_size + sets_size);
 
   c->lines_per_set = lines_per_set;
   c->block_bits = block_bits;
@@ -56,71 +54,68 @@ Cache *new_cache(size_t set_bits, size_t lines_per_set, size_t block_bits) {
   for (size_t i = 0; i < sets_capacity; i++) {
     Set *s = &c->sets[i];
     s->valid_lines = 0;
+    s->lines = &all_lines[i * lines_per_set];
     s->most_recent = 0;
     s->least_recent = 0;
-
-    s->lines = &all_lines[i * lines_per_set];
-
-    for (size_t j = 0; j < lines_per_set; j++) {
-      s->lines[j].tag = EMPTY_TAG;
-    }
   }
 
   return c;
 }
 
-void move_to_most_recent(Set *s, uint32_t line_index) {
+void move_to_most_recent(Set *s, uint32_t line_index, bool splice) {
   if (s->most_recent == line_index) {
     return;
   }
 
-  Line *next_most_recent = &s->lines[line_index];
-  next_most_recent->next = s->most_recent;
-  Line *previous_most_recent = &s->lines[s->most_recent];
-  previous_most_recent->previous = line_index;
+  Line *current = &s->lines[line_index];
+  if (s->least_recent == line_index) {
+    s->least_recent = current->previous;
+  } else if (splice)
+    s->lines[current->next].previous = current->previous;
 
+  if (splice) {
+    s->lines[current->previous].next = current->next;
+  }
+
+  current->next = s->most_recent;
+  s->lines[s->most_recent].previous = line_index;
   s->most_recent = line_index;
 }
 
 AccessStatus cache_upsert(Cache *c, uint64_t address) {
   AccessStatus st = {false, false, false};
 
-  uint32_t set_index = (address >> c->block_bits) & ((1UL << c->set_bits) - 1);
+  uint32_t set_index = (address >> c->block_bits) & ((1 << c->set_bits) - 1);
   uint64_t tag = address >> (c->block_bits + c->set_bits);
   Set *s = &c->sets[set_index];
 
-  Line *l;
-  uint32_t line_index = 0;
-  uint32_t free_slot_index = ~0;
-  for (; line_index < c->lines_per_set; line_index++) {
-    l = &s->lines[line_index];
-    if (l->tag == tag) {
-      move_to_most_recent(s, line_index);
+  uint32_t line_index = s->most_recent;
+  for (size_t i = 0; i < s->valid_lines; i++) {
+    Line *current = &s->lines[line_index];
+
+    if (current->tag == tag) {
+      move_to_most_recent(s, line_index, true);
       st.hit = true;
       return st;
     }
 
-    if (l->tag == EMPTY_TAG) {
-      free_slot_index = line_index;
+    if (i == c->lines_per_set - 1) {
+      st.eviction = true;
+      break;
     }
+
+    line_index = current->next;
   }
+
   st.miss = true;
-
-  if (free_slot_index == ~0) {
-    st.eviction = true;
-    line_index = s->least_recent;
-    l = &s->lines[line_index];
-    s->least_recent = l->previous;
-  } else {
-    line_index = free_slot_index;
-    if (s->valid_lines == 0)
-      s->least_recent = line_index;
+  if (!st.eviction) {
+    line_index = s->valid_lines;
     s->valid_lines++;
-    l = &s->lines[line_index];
   }
-  l->tag = tag;
 
-  move_to_most_recent(s, line_index);
+  Line *target = &s->lines[line_index];
+  target->tag = tag;
+  move_to_most_recent(s, line_index, st.eviction);
 
   return st;
 }
