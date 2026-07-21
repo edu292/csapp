2377130152e296fabc80a35274179cf3eb2de522
edu292 <eduskoroboatei@gmail.com>
2026-07-21 20:08:03 -0300

    Transpose with simd, against the rules but more useful

--- labs/cache/trans.c
+++ labs/cache/trans.c
@@ -8,7 +8,7 @@
  * on a 1KB direct mapped cache with a block size of 32 bytes.
  */
 #include "cachelab.h"
-#include <stdio.h>
+#include <immintrin.h>
 
 int min(int x, int y) { return x < y ? x : y; }
 
@@ -24,20 +24,52 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
-  int tile_size = 11;
+  __m256i row[8];
+  int i, j;
 
-  for (int tile_y = 0; tile_y < M; tile_y += tile_size) {
-    int tile_y_max = min(tile_y + tile_size, M);
+  for (i = 0; i <= N - 8; i += 8) {
+    for (j = 0; j <= M - 8; j += 8) {
 
-    int tile_x = 0;
-    while (tile_x < N) {
-      int tile_x_max = min(tile_x + tile_size, N);
-
-      for (; tile_x < tile_x_max; tile_x++) {
-        for (int y = tile_y; y < tile_y_max; y++) {
-          B[y][tile_x] = A[tile_x][y];
-        }
+      for (int k = 0; k < 8; k++) {
+        row[k] = _mm256_loadu_si256((const __m256i *)&A[i + k][j]);
       }
+
+      __m256i t0 = _mm256_unpacklo_epi32(row[0], row[1]);
+      __m256i t1 = _mm256_unpackhi_epi32(row[0], row[1]);
+      __m256i t2 = _mm256_unpacklo_epi32(row[2], row[3]);
+      __m256i t3 = _mm256_unpackhi_epi32(row[2], row[3]);
+      __m256i t4 = _mm256_unpacklo_epi32(row[4], row[5]);
+      __m256i t5 = _mm256_unpackhi_epi32(row[4], row[5]);
+      __m256i t6 = _mm256_unpacklo_epi32(row[6], row[7]);
+      __m256i t7 = _mm256_unpackhi_epi32(row[6], row[7]);
+
+      __m256i v0 = _mm256_unpacklo_epi64(t0, t2);
+      __m256i v1 = _mm256_unpackhi_epi64(t0, t2);
+      __m256i v2 = _mm256_unpacklo_epi64(t1, t3);
+      __m256i v3 = _mm256_unpackhi_epi64(t1, t3);
+      __m256i v4 = _mm256_unpacklo_epi64(t4, t6);
+      __m256i v5 = _mm256_unpackhi_epi64(t4, t6);
+      __m256i v6 = _mm256_unpacklo_epi64(t5, t7);
+      __m256i v7 = _mm256_unpackhi_epi64(t5, t7);
+
+      row[0] = _mm256_permute2x128_si256(v0, v4, 0x20);
+      row[1] = _mm256_permute2x128_si256(v1, v5, 0x20);
+      row[2] = _mm256_permute2x128_si256(v2, v6, 0x20);
+      row[3] = _mm256_permute2x128_si256(v3, v7, 0x20);
+      row[4] = _mm256_permute2x128_si256(v0, v4, 0x31);
+      row[5] = _mm256_permute2x128_si256(v1, v5, 0x31);
+      row[6] = _mm256_permute2x128_si256(v2, v6, 0x31);
+      row[7] = _mm256_permute2x128_si256(v3, v7, 0x31);
+
+      for (int k = 0; k < 8; k++) {
+        _mm256_storeu_si256((__m256i *)&B[j + k][i], row[k]);
+      }
+    }
+  }
+
+  for (int r = 0; r < N; r++) {
+    for (int c = (r < i) ? j : 0; c < M; c++) {
+      B[c][r] = A[r][c];
     }
   }
 }
